84f6de15f54145e5a58b1eccead53f08f8ecadea
o2ber <Oscar.De-Berranger@etu.univ-grenoble-alpes.fr>
2022-10-19 11:55:11 +0200

    update calculette fonctionnelle

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
 const chiffres = document.querySelectorAll('.bouton')
 const ecran = document.querySelector('.ecran')
 const boutonEgal = document.querySelector('.egal')
+const boutonClear = document.querySelector('.boutonClear')
 
 let resultat = 0
 ecran.innerHTML = resultat
 
 function calcule(){
-
+    resultat = eval(ecran.innerHTML)
+    ecran.innerHTML = resultat
 }
+boutonClear.addEventListener('click', ()=>{
+    ecran.innerHTML = 0
+})
+
+
 
 chiffres.forEach((chiffre)=>{
     chiffre.addEventListener('click',()=>{
-        console.log(chiffre.id)
-        ecran.innerHTML += chiffre.id
+        if (ecran.innerHTML=='0'){
+            ecran.innerHTML = chiffre.id
+            console.log(chiffre.id)
+        }else {
+            console.log(chiffre.id)
+            ecran.innerHTML += chiffre.id
+        }
 
     })
 })
 
 boutonEgal.addEventListener('click',()=>{
     calcule();
-
 })
